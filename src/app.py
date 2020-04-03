90114662a5df543c57494b40508dcdc4a745d504
Britton Lee <blee13@vt.edu>
2020-04-03 15:09:22 -0400

    MongoDB working

--- src/app.py
+++ src/app.py
@@ -1,14 +1,24 @@
 from flask import Flask, jsonify, make_response, json, request
 from time import time
 from uuid import uuid4
+import pymongo
+from bson.objectid import ObjectId
 
 app=Flask(__name__)
 
-blabs = []
+#blabs = []
 #{"id": "ASDHF872HF", "postTime": 55555, "author": {"email": "briton.lee", "name": "Britton"}, "message": "My first blab!"}
+mongoClient = pymongo.MongoClient("mongodb://mongo:27017")
+mongoCollection = mongoClient["cs2304"]["blabs"]
 
 @app.route('/blabs', methods=['GET'])
 def get_all_blabs():
+  blabs = []
+  for aBlab in mongoCollection.find():
+    blab = aBlab.copy()
+    blab["id"] = str(blab["_id"])
+    del blab["_id"]
+    blabs.append(blab)
   createdSince = 0
   #Check url params for createdSince and set variable accordingly
   if request.args.get("createdSince"):
@@ -33,22 +43,30 @@ def add_blab():
   author = content["author"]
   message = content["message"]
   #Create a uuid and retrieve the time for the blab
-  uuid = str(uuid4())
+  #uuid = str(uuid4())
   postTime = int(time())
   #Create the actual blab, add it to blabs, and return successful
-  newBlab = {"id": uuid, "postTime": postTime, "author": author, "message": message}
-  blabs.append(newBlab)
+  newBlab = {"postTime": postTime, "author": author, "message": message}
+  response = mongoCollection.insert_one(newBlab)
+  #blabs.append(newBlab)
+  newBlab = {"postTime": postTime, "author": author, "message": message}
+  newBlab["id"] = str(response.inserted_id)
   return make_response(jsonify(newBlab), 201)
+  #return make_response(jsonify(newBlab), 201)
 
 
 @app.route('/blabs/<string:identity>', methods=['DELETE'])
 def delete(identity):
+  result = mongoCollection.delete_one({"_id": ObjectId(identity)})
   #Loop through array of JSON blabs, look for id match
-  for blab in blabs:
-    uuid = json.loads(json.dumps(blab))["id"]
-    if identity == uuid:
-      #If found, remove from blabs and return successful
-      blabs.remove(blab)
-      return make_response(jsonify({"message": "Blab deleted successfully."}), 200)
+  #for blab in blabs:
+  #  uuid = json.loads(json.dumps(blab))["id"]
+  #  if identity == uuid:
+  #    #If found, remove from blabs and return successful
+  #    blabs.remove(blab)
+  print(result.deleted_count)
+  if result.deleted_count > 0:
+    return make_response(jsonify({"message": "Blab deleted successfully."}), 200)
   #Otherwise, return unsuccessful
-  return make_response(jsonify({"message": "Blab not found."}), 404)
+  else:
+    return make_response(jsonify({"message": "Blab not found."}), 404)
