fda0cbd19b8a13db99d37844328becae5bb5d89c
Britton Lee <blee13@vt.edu>
2020-04-10 11:31:50 -0400

    Added prometheus and grafana capabilities

--- src/app.py
+++ src/app.py
@@ -3,11 +3,11 @@ from time import time
 from uuid import uuid4
 import pymongo
 from bson.objectid import ObjectId
+from prometheus_flask_exporter import PrometheusMetrics
 
 app=Flask(__name__)
+metrics = PrometheusMetrics(app)
 
-#blabs = []
-#{"id": "ASDHF872HF", "postTime": 55555, "author": {"email": "briton.lee", "name": "Britton"}, "message": "My first blab!"}
 mongoClient = pymongo.MongoClient("mongodb://mongo:27017")
 mongoCollection = mongoClient["cs2304"]["blabs"]
 
@@ -37,33 +37,24 @@ def get_all_blabs():
 
 
 @app.route('/blabs', methods = ['POST'])
+@metrics.counter('new_blab', 'Total number of blabs created.')
 def add_blab():
   #Retrieve author and message from the content of the request
   content = request.json
   author = content["author"]
   message = content["message"]
-  #Create a uuid and retrieve the time for the blab
-  #uuid = str(uuid4())
   postTime = int(time())
   #Create the actual blab, add it to blabs, and return successful
   newBlab = {"postTime": postTime, "author": author, "message": message}
   response = mongoCollection.insert_one(newBlab)
-  #blabs.append(newBlab)
   newBlab = {"postTime": postTime, "author": author, "message": message}
   newBlab["id"] = str(response.inserted_id)
   return make_response(jsonify(newBlab), 201)
-  #return make_response(jsonify(newBlab), 201)
 
 
 @app.route('/blabs/<string:identity>', methods=['DELETE'])
 def delete(identity):
   result = mongoCollection.delete_one({"_id": ObjectId(identity)})
-  #Loop through array of JSON blabs, look for id match
-  #for blab in blabs:
-  #  uuid = json.loads(json.dumps(blab))["id"]
-  #  if identity == uuid:
-  #    #If found, remove from blabs and return successful
-  #    blabs.remove(blab)
   print(result.deleted_count)
   if result.deleted_count > 0:
     return make_response(jsonify({"message": "Blab deleted successfully."}), 200)
